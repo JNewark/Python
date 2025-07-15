682534f7914a585c0f4507ff6646cc36450916bd
John Newark <johnathan.newark@gmail.com>
2025-07-14 21:35:04 -0400

    Add files via upload

---
+++ BandNameGenerator.py
@@ -0,0 +1,9 @@
+
+print("Welcome to your Band Name Generator!")
+# input("What City did you grow up in?")
+# input("What is your pet's name?")
+
+city = input("What city did you grow up in?\n")
+pet = input("What is your pet's name?\n")
+
+print("Your band name could be: " + city, pet)
